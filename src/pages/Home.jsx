c49f6ee594be622185c0272ba4e9265263bc4789
Mathusan Aiyadurai Iyer <22655198+Mathu145@users.noreply.github.com>
2025-05-13 22:22:38 +0200

    first section created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { HeroSection } from "../components/HeroSection";
 import { Navbar } from "../components/Navbar";
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "@/components/StarBackground";
@@ -11,7 +12,9 @@ export const Home = () => {
         {/* Navbar */}
             <Navbar />
         {/* Main Content */}
-
+            <main>
+                <HeroSection />
+            </main>
 
         {/* Footer */}
     </div>;
