25ebfbb28c5b7775e70d9ae451dc4b18befa0a4f
Mathusan Aiyadurai Iyer <22655198+Mathu145@users.noreply.github.com>
2025-05-16 19:14:52 +0200

    Career&EducationPath added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { AboutSection } from "../components/AboutSection";
+import { CareerEducationSection } from "../components/CareerEducationSection";
 import { HeroSection } from "../components/HeroSection";
 import { Navbar } from "../components/Navbar";
 import { SkillsSection } from "../components/SkillsSection";
@@ -18,6 +19,7 @@ export const Home = () => {
                 <HeroSection />
                 <AboutSection />
                 <SkillsSection />
+                <CareerEducationSection />
             </main>
 
         {/* Footer */}
