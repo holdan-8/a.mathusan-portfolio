f37f0ba6ae3384542e1867f4e87ad0ae75397cce
Mathusan Aiyadurai Iyer <22655198+Mathu145@users.noreply.github.com>
2025-05-16 23:15:36 +0200

    add changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { cn } from "../lib/utils";
+import { cn } from "@/lib/utils";
 import { Menu } from "lucide-react";
 import { X } from "lucide-react";
 import { useEffect } from "react";
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,10 @@
 import { AboutSection } from "../components/AboutSection";
 import { CareerEducationSection } from "../components/CareerEducationSection";
-<<<<<<< HEAD
-import { ContactSection } from "../components/ContactSection";
-import { Footer } from "../components/Footer";
-=======
->>>>>>> parent of 34880a8 (add the contactsection)
 import { HeroSection } from "../components/HeroSection";
 import { Navbar } from "../components/Navbar";
 import { SkillsSection } from "../components/SkillsSection";
-import { ThemeToggle } from "../components/ThemeToggle";
-import { StarBackground } from "../components/StarBackground";
+import { ThemeToggle } from "@/components/ThemeToggle";
+import { StarBackground } from "@/components/StarBackground";
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
