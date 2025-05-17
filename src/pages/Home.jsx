8f655a7f56312a3f3293858d6217788685946209
Mathusan Aiyadurai Iyer <22655198+Mathu145@users.noreply.github.com>
2025-05-17 16:39:54 +0200

    add some chages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+export const Home = () => {
+    return (
+      <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+        {/* Theme Toggle */}
+  
+        {/* Background Effects */}
+  
+        {/* Navbar */}
+  
+        {/* Main Content */}
+        <main>
+          <div>HOME</div>
+        </main>
+  
+        {/* Footer */}
+  
+      </div>
+    );
+  };
+  
+  
